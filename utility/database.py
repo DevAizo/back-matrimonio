302cbec55714b00566b4b43664552e2b7a135b1b
bdesarrolloFIMLM <b.desarrollo@fundacionmarialuisa.org>
2026-03-25 18:50:47 -0500

    fix database env

--- utility/database.py
+++ utility/database.py
@@ -1,9 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:bOjTabdpJfJlxduoQkqSqvMOjbVVjqCC@caboose.proxy.rlwy.net:11113/railway"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
